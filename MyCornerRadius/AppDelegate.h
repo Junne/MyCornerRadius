2c4dac84166b37b94a609fc811f3128c8bb23707
Junne <junne.bai@gmail.com>
2016-03-28 19:12:03 +0800

    Initial Commit

---
+++ MyCornerRadius/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyCornerRadius
+//
+//  Created by baijf on 3/28/16.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyCornerRadius/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyCornerRadius
+//
+//  Created by baijf on 3/28/16.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
