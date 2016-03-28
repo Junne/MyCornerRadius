85d2cc1815edae14d45929603dd92318cbfdb0a5
Junne <junne.bai@gmail.com>
2016-03-28 20:02:56 +0800

    init project

---
+++ MyCornerRadius/UIImageView+CornerRadius.h
@@ -0,0 +1,13 @@
+//
+//  UIImageView+CornerRadius.h
+//  MyCornerRadius
+//
+//  Created by baijf on 3/28/16.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImageView (CornerRadius)
+
+@end
---
+++ MyCornerRadius/UIImageView+CornerRadius.m
@@ -0,0 +1,13 @@
+//
+//  UIImageView+CornerRadius.m
+//  MyCornerRadius
+//
+//  Created by baijf on 3/28/16.
+//  Copyright © 2016 Junne. All rights reserved.
+//
+
+#import "UIImageView+CornerRadius.h"
+
+@implementation UIImageView (CornerRadius)
+
+@end
